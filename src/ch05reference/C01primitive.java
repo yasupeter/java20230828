fe34970738b3c817bbf4eb86e4c2a2d5e5fd6a01
yasupeter <peterleeful@gmail.com>
2023-08-28 15:44:07 +0900

    객체와 클래스

--- src/ch05reference/C01primitive.java
+++ src/ch05reference/C01primitive.java
@@ -1,10 +1,10 @@
 package ch05reference;
 
 public class C01primitive {
-	public static void main(Strings[] args) {
+	public static void main(String[] args) {
 		int a;   // 변수 선언
 		a=3; // 데이터타입(자료형)에 맞는 값만 대입 가능
-		a=3.14; // 데이터타입에 맞지 않다면 대입 불가능
+		 // 데이터타입에 맞지 않다면 대입 불가능
 	
 		int b=5; // 선언과 동시에 값 대입
 		
